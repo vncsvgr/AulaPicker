114e6757659a9d34e2cf533a43b63a134dc24c36
Usuario <Usuario@CPD03-M12>
2022-03-23 22:27:15 -0300

    Adicionar arquivos de projeto.

---
+++ AulaPicker/AulaPicker/Curso.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AulaPicker
+{
+    public class Curso
+    {
+        public string Nome { get; set; }
+        public string Periodo { get; set; }
+    }
+}
---
+++ AulaPicker/AulaPicker/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AulaPicker
+{
+    public partial class MainPage : ContentPage
+    {
+        List<string> estadoCivil = new List<string>
+        {
+            "Casado",
+            "Solteiro",
+            "Viúvo",
+            "Divorciado"
+        };
+
+        List<Curso> curso = new List<Curso>
+        {
+            new Curso {Nome="DESENVOLVIMENTO", Periodo="NOTURNO"},
+            new Curso {Nome="ADMINISTRACAO", Periodo="NOTURNO"},
+            new Curso {Nome="NUTRICAO", Periodo="NOTURNO"},
+            new Curso {Nome="ETIM MECATRONICA", Periodo="DIURNO"},
+            new Curso {Nome="ETIM EDIFICACOES", Periodo="DIURNO"}
+        };
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            pckEstadoCivil.ItemsSource = estadoCivil;
+
+            pckCurso.ItemsSource = curso;
+        }
+
+        private void pckEstadoCivil_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DisplayAlert("Mensagem", pckEstadoCivil.SelectedItem.ToString(), "OK");
+        }
+
+        private void btnVerificar_Clicked(object sender, EventArgs e)
+        {
+            Curso c = curso.Find(x => x.Nome == pckCurso.Items[pckCurso.SelectedIndex].ToString());
+            lblCurso.Text = c.Nome;
+            lblPeriodo.Text = c.Periodo;
+        }
+    }
+}
